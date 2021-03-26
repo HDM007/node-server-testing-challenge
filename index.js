90775a832b7f41bf54217d08c47a188de88f6310
hdm007 <harry.mardirossian13@gmail.com>
2021-03-26 02:01:17 -0400

    added index, set up routes next

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const server = require("./api/server");
+
+const PORT = process.env.port || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}, via Index file.`)
+});
