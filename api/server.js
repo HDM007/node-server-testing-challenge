e5f88c9e1edbb3b1ac2049ab40fa7bb638883c16
hdm007 <harry.mardirossian13@gmail.com>
2021-03-26 01:58:35 -0400

    finished server export, now work on route

--- api/server.js
+++ api/server.js
@@ -6,3 +6,9 @@ server.use(helmet());
 server.use(express.json());
 
 //server.use("api/snacks", snacksRouter)
+
+server.get("/", (req, res) => {
+    res.status(200).json({message: "Server is running"});
+});
+
+module.exports = server;
